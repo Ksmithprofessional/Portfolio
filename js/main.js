6b23ee8164512d7782e736e0d2df382fc291c337
Karl Smith <98028208+Ksmithprofessional@users.noreply.github.com>
2022-04-12 14:25:48 +0100

    Created main.js, planned out what to do

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// menu bar 
+
+// create html and css for menu at small Screens
+// add event listener to button click
+// if button clicked, show menu bar, hide menu button
+// if button clicked again, hide menu bar (display none), bring back menu button
+
+// text animation on the header text
+
+
+// form validation
+//if required fields have collected input then proceed (input.value = true?)
+// else display "fill in required fields"
+// also check the formatting of email and phone number fields
+// if phone number/email format = true, proceed
+// else "not a valid email/phone number"
